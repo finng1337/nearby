eaee3820e01ef5320f504d4048cf2793e18ec4fe
Radek Čermák <cermara7@cvut.cz>
2024-03-25 14:50:41 +0100

    Added caching

--- app/src/db/actions/scheduleActions.ts
+++ app/src/db/actions/scheduleActions.ts
@@ -4,6 +4,7 @@ import db from "@/db/drizzle";
 import {schedule} from "@/db/schema";
 import {Schedule, InsertSchedule, GetScheduleResponse, GetSchedulesResponse, GetSchedulesFilters} from "@/db/types";
 import {and, asc, eq, gt, inArray} from "drizzle-orm";
+import {unstable_cache} from "next/cache";
 
 export const getScheduleIds = async (): Promise<{id: number; idGoout: number | null; idKudyznudy: string | null}[]> => {
     return db
@@ -14,97 +15,109 @@ export const getScheduleIds = async (): Promise<{id: number; idGoout: number | n
         })
         .from(schedule);
 };
-export const getSchedules = async (filters: GetSchedulesFilters): Promise<GetSchedulesResponse> => {
-    let filtersArray = [];
+export const getSchedules = unstable_cache(
+    async (filters: GetSchedulesFilters): Promise<GetSchedulesResponse> => {
+        let filtersArray = [];
 
-    if (filters.active) {
-        filtersArray.push(gt(schedule.endAt, new Date()));
-    }
-    if (filters.venueIds) {
-        filtersArray.push(inArray(schedule.venue, filters.venueIds));
-    }
+        if (filters.active) {
+            filtersArray.push(gt(schedule.endAt, new Date()));
+        }
+        if (filters.venueIds) {
+            filtersArray.push(inArray(schedule.venue, filters.venueIds));
+        }
 
-    let filter = undefined;
-    if (filtersArray.length === 1) {
-        filter = filtersArray[0];
-    } else if (filtersArray.length > 1) {
-        filter = and(...filtersArray);
-    }
+        let filter = undefined;
+        if (filtersArray.length === 1) {
+            filter = filtersArray[0];
+        } else if (filtersArray.length > 1) {
+            filter = and(...filtersArray);
+        }
 
-    return db.query.schedule.findMany({
-        where: filter,
-        columns: {
-            id: true,
-            startAt: true,
-            endAt: true,
-        },
-        with: {
-            event: {
-                columns: {
-                    title: true,
-                    images: true,
-                },
-                with: {
-                    category: {
-                        columns: {
-                            value: true,
+        return db.query.schedule.findMany({
+            where: filter,
+            columns: {
+                id: true,
+                startAt: true,
+                endAt: true,
+            },
+            with: {
+                event: {
+                    columns: {
+                        title: true,
+                        images: true,
+                    },
+                    with: {
+                        category: {
+                            columns: {
+                                value: true,
+                            },
                         },
                     },
                 },
-            },
-            venue: {
-                columns: {
-                    title: true,
-                },
-            },
-        },
-        orderBy: [asc(schedule.startAt)],
-    });
-};
-export const getSchedule = async (id: number): Promise<GetScheduleResponse> => {
-    return db.query.schedule.findFirst({
-        where: eq(schedule.id, id),
-        columns: {
-            id: true,
-            urlGoout: true,
-            urlKudyznudy: true,
-            startAt: true,
-            endAt: true,
-        },
-        with: {
-            event: {
-                columns: {
-                    title: true,
-                    description: true,
-                    images: true,
-                },
-                with: {
-                    category: {
-                        columns: {
-                            value: true,
-                            title: true,
-                        },
+                venue: {
+                    columns: {
+                        title: true,
                     },
-                    tags: {
-                        with: {
-                            tag: {
-                                columns: {
-                                    id: true,
-                                    title: true,
+                },
+            },
+            orderBy: [asc(schedule.startAt)],
+        });
+    },
+    ["getSchedules"],
+    {
+        revalidate: 60 * 60 * 24, // 24 hours
+    }
+);
+export const getSchedule = unstable_cache(
+    async (id: number): Promise<GetScheduleResponse> => {
+        return db.query.schedule.findFirst({
+            where: eq(schedule.id, id),
+            columns: {
+                id: true,
+                urlGoout: true,
+                urlKudyznudy: true,
+                startAt: true,
+                endAt: true,
+            },
+            with: {
+                event: {
+                    columns: {
+                        title: true,
+                        description: true,
+                        images: true,
+                    },
+                    with: {
+                        category: {
+                            columns: {
+                                value: true,
+                                title: true,
+                            },
+                        },
+                        tags: {
+                            with: {
+                                tag: {
+                                    columns: {
+                                        id: true,
+                                        title: true,
+                                    },
                                 },
                             },
                         },
                     },
                 },
-            },
-            venue: {
-                columns: {
-                    title: true,
+                venue: {
+                    columns: {
+                        title: true,
+                    },
                 },
             },
-        },
-    });
-};
+        });
+    },
+    ["getSchedule"],
+    {
+        revalidate: false,
+    }
+);
 export const addSchedule = async (insertSchedule: InsertSchedule): Promise<Schedule> => {
     const insertedData = await db.insert(schedule).values(insertSchedule).returning();
 
--- app/src/db/actions/venueActions.ts
+++ app/src/db/actions/venueActions.ts
@@ -4,6 +4,7 @@ import {GetVenuesFilters, GetVenuesResponse, InsertVenue, Venue} from "@/db/type
 import db from "@/db/drizzle";
 import {schedule, venue} from "@/db/schema";
 import {gt} from "drizzle-orm";
+import {unstable_cache} from "next/cache";
 
 export const getVenuesIds = async (): Promise<{id: number; idGoout: number | null; idKudyznudy: string | null}[]> => {
     return db
@@ -14,40 +15,46 @@ export const getVenuesIds = async (): Promise<{id: number; idGoout: number | nul
         })
         .from(venue);
 };
-export const getVenues = async (filters: GetVenuesFilters): Promise<GetVenuesResponse> => {
-    const data = await db.query.venue.findMany({
-        columns: {
-            id: true,
-            lat: true,
-            lon: true,
-        },
-        with: {
-            schedules: {
-                columns: {
-                    id: true,
-                },
-                where: filters.active ? gt(schedule.endAt, new Date()) : undefined,
-                with: {
-                    event: {
-                        columns: {
-                            id: true,
-                        },
-                        with: {
-                            category: {
-                                columns: {
-                                    id: true,
-                                    value: true,
+export const getVenues = unstable_cache(
+    async (filters: GetVenuesFilters): Promise<GetVenuesResponse> => {
+        const data = await db.query.venue.findMany({
+            columns: {
+                id: true,
+                lat: true,
+                lon: true,
+            },
+            with: {
+                schedules: {
+                    columns: {
+                        id: true,
+                    },
+                    where: filters.active ? gt(schedule.endAt, new Date()) : undefined,
+                    with: {
+                        event: {
+                            columns: {
+                                id: true,
+                            },
+                            with: {
+                                category: {
+                                    columns: {
+                                        id: true,
+                                        value: true,
+                                    },
                                 },
                             },
                         },
                     },
                 },
             },
-        },
-    });
+        });
 
-    return filters.active ? data.filter(({schedules}) => schedules.length > 0) : data;
-};
+        return filters.active ? data.filter(({schedules}) => schedules.length > 0) : data;
+    },
+    ["getVenues"],
+    {
+        revalidate: 60 * 60 * 3600, // 24 hours
+    }
+);
 export const addVenue = async (insertVenue: InsertVenue): Promise<Venue> => {
     const insertedData = await db.insert(venue).values(insertVenue).returning();
 
